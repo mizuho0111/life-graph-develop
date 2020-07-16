79b094716e310649275c9389f25dae118a57f0c5
mizuho.kawashima <mizuho.kawashima@seattleconsulting.co.jp>
2020-07-16 15:30:06 +0900

    main.js test

--- vue/src/main.js
+++ vue/src/main.js
@@ -6,6 +6,7 @@ import App from '@/App'
 import router from '@/router'
 import store from '@/store'
 // import lang from '@/lang'
+// test
 
 // Vue.use(VeeValidate)
 Vue.config.productionTip = false
